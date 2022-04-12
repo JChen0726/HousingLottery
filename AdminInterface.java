706230cffbf30941837f2bd8ddf157338f7209fc
Bryn Kerslake <brynkerslake@gmail.com>
2022-04-11 20:03:50 -0400

    Update AdminInterface.java

--- AdminInterface.java
+++ AdminInterface.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class AdminInterface {
 
     AdminInterface(){
-        System.out.println("Welcom - admin");
+        System.out.println("Welcome, ");
     }
 
     public void startGolottery (){
